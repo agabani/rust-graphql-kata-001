ffeb79973e295e22ef9a1497ba1610c2d50e6d66
agabani <agabani@users.noreply.github.com>
2021-07-09 20:46:51 +0100

    feat: add graphql playground

--- rust-graphql-kata-001-web/src/routes/graphql.rs
+++ rust-graphql-kata-001-web/src/routes/graphql.rs
@@ -1,4 +1,5 @@
-use actix_web::{web, HttpResponse};
+use actix_web::{web, HttpResponse, Result};
+use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.route("/", web::post().to(index))
@@ -9,6 +10,10 @@ async fn index() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-async fn index_playground() -> HttpResponse {
-    HttpResponse::Ok().finish()
+async fn index_playground() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(playground_source(
+            GraphQLPlaygroundConfig::new("/").subscription_endpoint("/"),
+        )))
 }
