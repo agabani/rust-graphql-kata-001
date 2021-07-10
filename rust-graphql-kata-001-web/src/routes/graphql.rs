1f31f1f5d00fd2431a47e046f01408131790549e
agabani <agabani@users.noreply.github.com>
2021-07-10 04:28:50 +0100

    feat: add graphql execution

--- rust-graphql-kata-001-web/src/routes/graphql.rs
+++ rust-graphql-kata-001-web/src/routes/graphql.rs
@@ -1,19 +1,21 @@
-use actix_web::{web, HttpResponse, Result};
+use crate::graphql::GraphQLSchema;
+use actix_web::{web, HttpResponse};
 use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
+use async_graphql::Request;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.route("/", web::post().to(index))
         .route("/", web::get().to(index_playground));
 }
 
-async fn index() -> HttpResponse {
-    HttpResponse::Ok().finish()
+async fn index(schema: web::Data<GraphQLSchema>, request: web::Json<Request>) -> HttpResponse {
+    HttpResponse::Ok().json(schema.execute(request.0).await)
 }
 
-async fn index_playground() -> Result<HttpResponse> {
-    Ok(HttpResponse::Ok()
+async fn index_playground() -> HttpResponse {
+    HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(playground_source(
-            GraphQLPlaygroundConfig::new("/").subscription_endpoint("/"),
-        )))
+            GraphQLPlaygroundConfig::new("/graphql/").subscription_endpoint("/graphql/"),
+        ))
 }
