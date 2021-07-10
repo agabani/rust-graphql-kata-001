d6e5a8b6ce826084346c2ecbb14d2ff721c07d71
agabani <agabani@users.noreply.github.com>
2021-07-10 20:28:40 +0100

    feat: add forum schema

--- migrations/20210710115018_initial.sql
+++ migrations/20210710115018_initial.sql
@@ -17,7 +17,7 @@ create table session
 (
     id         bigserial                not null,
     public_id  varchar(36)              not null,
-    user_id    integer                  not null,
+    user_id    bigint                   not null,
     user_agent varchar(200)             not null,
     created    timestamp with time zone not null,
     constraint session_pk
@@ -29,4 +29,58 @@ create table session
 
 create unique index session_public_id_uindex
     on session (public_id);
+
+create table forum
+(
+    id                 bigserial                not null,
+    public_id          varchar(36)              not null,
+    created            timestamp with time zone not null,
+    created_by_user_id bigint                   not null,
+    name               varchar(50)              not null,
+    constraint forum_pk
+        primary key (id),
+    constraint forum_user_id_fk
+        foreign key (created_by_user_id) references "user"
+);
+
+create unique index forum_public_id_uindex
+    on forum (public_id);
+
+create table thread
+(
+    id                 bigserial                not null,
+    public_id          varchar(36)              not null,
+    created            timestamp with time zone not null,
+    created_by_user_id bigint,
+    name               varchar(50)              not null,
+    forum_id           bigint                   not null,
+    constraint thread_pk
+        primary key (id),
+    constraint thread_user_id_fk
+        foreign key (created_by_user_id) references "user",
+    constraint thread_forum_id_fk
+        foreign key (forum_id) references forum
+);
+
+create unique index thread_public_id_uindex
+    on thread (public_id);
+
+create table reply
+(
+    id                 bigserial                not null,
+    public_id          varchar(36),
+    created            timestamp with time zone not null,
+    created_by_user_id bigint                   not null,
+    thread_id          bigint                   not null,
+    text               text                     not null,
+    constraint reply_pk
+        primary key (id),
+    constraint reply_user_id_fk
+        foreign key (created_by_user_id) references "user",
+    constraint reply_thread_id_fk
+        foreign key (thread_id) references thread
+);
+
+create unique index reply_public_id_uindex
+    on reply (public_id);
 
