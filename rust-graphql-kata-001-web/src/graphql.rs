20e8a9923d4846c1dcdd77d5eaffc003857517dd
agabani <agabani@users.noreply.github.com>
2021-07-10 12:16:19 +0100

    feat: add connection

--- rust-graphql-kata-001-web/src/graphql.rs
+++ rust-graphql-kata-001-web/src/graphql.rs
@@ -1,5 +1,6 @@
 use crate::domain::{Session, User, UserAgent, Username};
-use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema};
+use async_graphql::connection::{query, Connection, Edge, EmptyFields};
+use async_graphql::{EmptyMutation, EmptySubscription, Object, Result, Schema};
 
 pub type GraphQLSchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
 
@@ -53,6 +54,40 @@ impl User {
                 .cloned()
                 .collect::<Vec<_>>(),
         }
+    }
+
+    async fn numbers(
+        &self,
+        after: Option<String>,
+        before: Option<String>,
+        first: Option<i32>,
+        last: Option<i32>,
+    ) -> Result<Connection<usize, i32, EmptyFields, EmptyFields>> {
+        query(
+            after,
+            before,
+            first,
+            last,
+            |after, before, first, last| async move {
+                let mut start = after.map(|after| after + 1).unwrap_or(0);
+                let mut end = before.unwrap_or(10000);
+                if let Some(first) = first {
+                    end = (start + first).min(end);
+                }
+                if let Some(last) = last {
+                    start = if last > end - start { end } else { end - last };
+                }
+
+                let mut connection = Connection::new(start > 0, end < 10000);
+                connection.append(
+                    (start..end)
+                        .into_iter()
+                        .map(|n| Edge::with_additional_fields(n, n as i32, EmptyFields)),
+                );
+                Ok(connection)
+            },
+        )
+        .await
     }
 }
 
